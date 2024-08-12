8a33dbf2e5cf5261b8f448853762fb87bebc6717
anonim00-hack <m65334287@gmail.com>
2024-08-12 20:21:01 +0500

    header

--- app.js
+++ app.js
@@ -3,30 +3,42 @@ const enterBtn = document.querySelector('.enter');
 const HTML = document.querySelector('html');
 const BODY = document.querySelector('body');
 const loading = document.querySelector('.loading');
-const bodyContainer = document.querySelector('.body_container');
+const htmlContainer = document.querySelector('.html_container');
 const players = document.querySelectorAll('.players');
+
+const header = document.querySelector('.header')
+const icon = document.querySelector('.icon2')
+const searchInp = document.querySelector('.search_inp')
+
+window.addEventListener('scroll',()=>{
+    if (window.scrollY > 100) {
+        header.style.animation = 'pos 0.6s forwards'
+        header.style.position = 'fixed'
+        searchInp.style.position = 'absolute'
+        icon.style.visibility = 'hidden';
+    } else {
+        header.style.animation = 'opacity 0.6s forwards'
+        header.style.position = 'absolute'
+        searchInp.style.position = 'static'
+        icon.style.visibility = 'visible';
+    }
+})
 
 window.addEventListener('load', () => {
     enterBtn.style.transform = 'translateX(0px)';
 });
 
-let background = 0;
-
 enterBtn.addEventListener('click', () => {
     welcome.style.display = 'none';
     loading.style.display = 'block';
-    bodyContainer.style.display = 'flex';
-    if (background == 0) {
-        BODY.classList.add('background1')
-        HTML.style.animation = 'opacity2 1s forwards'
-    }
+    htmlContainer.style.display = 'flex';
     checkIfImagesLoaded(); // Проверить, загружены ли уже изображения
 });
 
 function checkIfImagesLoaded() {
     let loadedCount = 0;
     const totalImages = players.length;
-
+    
     players.forEach(player => {
         if (player.complete) {
             // Если изображение уже загружено
@@ -37,15 +49,15 @@ function checkIfImagesLoaded() {
                 loadedCount++;
                 if (loadedCount === totalImages) {
                     loading.style.display = 'none';
-                    bodyContainer.classList.add('opacity2');
+                    htmlContainer.classList.add('opacity2');
                 }
             };
         }
     });
-
+    
     // Если все изображения были загружены до регистрации обработчиков
     if (loadedCount === totalImages) {
         loading.style.display = 'none';
-        bodyContainer.classList.add('opacity2');
+        htmlContainer.classList.add('opacity2');
     }
 }
